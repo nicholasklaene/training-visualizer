3067f508435d7e2ec4306b5a51d58e1cf49c3b5a
Nicholas Klaene <nicholasklaene@outlook.com>
2021-04-20 18:50:47 -0400

    lambda function now saves access token to AWS secrets manager

--- lambdas/rotateKeys/index.js
+++ lambdas/rotateKeys/index.js
@@ -1,41 +1,54 @@
 exports.handler = async (event) => {
-    
+
     const fetch = require("node-fetch");
+    const SecretsManager = require("aws-sdk/clients/secretsmanager");
+
+    const client = new SecretsManager({
+        region: "us-east-2"
+    });
 
     let refreshToken = process.env.REFRESH_TOKEN;
     let clientSecret = process.env.CLIENT_SECRET;
     let clientId = process.env.CLIENT_ID;
-    
+
     const url = "https://www.strava.com/oauth/token";
 
-    let data = await fetch(url, { 
+    let data = await fetch(url, {
         method: "POST",
         headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             client_id: clientId,
             client_secret: clientSecret,
             refresh_token: refreshToken,
-            grant_type: 'refresh_token'
-        }) 
+            grant_type: "refresh_token"
+        })
     });
-
+    
     data = await data.json();
 
-    let accessToken = data.access_token;
+    const accessToken = data.access_token;
 
-    // store in DB
+    // store in secrets manager
+    const params = {
+        SecretId: "access_token",
+        SecretString: JSON.stringify(accessToken)
+    };
 
-    // return 204 when it is verified to work
+    client.updateSecret(params, (err, data) => {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log(data);
+        }
+    });
+
     const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            accessToken: accessToken
-        }),
+        statusCode: 204
     };
 
     return response;
-    
+
 };
