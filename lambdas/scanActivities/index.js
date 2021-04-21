1b22a6fcdc54c1c93c78fac6ca6305d15cd989a0
Nicholas Klaene <nicholasklaene@outlook.com>
2021-04-21 08:47:14 -0400

    finished scanActivities service

--- lambdas/scanActivities/index.js
+++ lambdas/scanActivities/index.js
@@ -12,45 +12,71 @@ exports.handler = async (event) => {
     let accessToken = data.SecretString
     accessToken = accessToken.substring(1, accessToken.length - 1)
 
-    let stravaResponseActivities = await fetch("https://www.strava.com/api/v3/athlete/activities?page=1&per_page=10", {
+    let stravaResponseActivities = await fetch("https://www.strava.com/api/v3/athlete/activities?page=1&per_page=5", {
         headers: {
             "Authorization": `Bearer ${accessToken}`
         },
-    });
+    })
 
     stravaResponseActivities = await stravaResponseActivities.json()
 
     let activities = []
 
-    stravaResponseActivities.forEach(activity => {
+    for (const activity of stravaResponseActivities) {
 
-        let item = {
-            activity_id: activity.id,
-            type: activity.type,
-            name: activity.name,
-            distance: activity.distance,
-            moving_time: activity.moving_time,
-            polyline: activity.map.summary_polyline,
-            elapsed_time: activity.elapsed_time
+        console.log(activity.id)
+        params = {
+            TableName: "training-visualizer",
+            Key: {
+                activity_id: activity.id
+            },
+            AttributesToGet: [
+                'activity_id'
+            ]
         }
 
-        let current = {
-            PutRequest: {
-                Item: item
+        let result = await dynamodb.get(params).promise() 
+
+        if (result.Item === null || result.Item === undefined) {
+
+            let item = {
+                activity_id: activity.id,
+                type: activity.type,
+                name: activity.name,
+                distance: activity.distance,
+                moving_time: activity.moving_time,
+                polyline: activity.map.summary_polyline,
+                elapsed_time: activity.elapsed_time,
+                date: activity.start_date_local,
+                elevation_gain: activity.total_elevation_gain,
+                start_latitude: activity.start_latitude,
+                start_longitude: activity.start_longitude
+
             }
-        }
 
-        activities.push(current)
-    });
+            let current = {
+                PutRequest: {
+                    Item: item
+                }
+            }
+            console.log("pushing activity " + activity.id + " to DB")
+            activities.push(current)
 
-    params = {
-        RequestItems: {
-            "training-visualizer": activities
+        } else {
+            console.log("activity " + activity.id + " already in DB")
         }
     }
 
-
-    await dynamodb.batchWrite(params).promise()
+    if (activities.length === 0) {
+        console.log("No new activities found")
+    } else {
+        params = {
+            RequestItems: {
+                "training-visualizer": activities
+            }
+        }
+        await dynamodb.batchWrite(params).promise()
+    }
 
     const response = {
         statusCode: 204
