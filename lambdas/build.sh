e4cb7f6c04ac20f4da440a85738ecdf9dc5f09da
Nicholas Klaene <nicholasklaene@outlook.com>
2021-04-21 10:00:46 -0400

    created getActivities service

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -13,4 +13,10 @@ zip -r ../scanActivities.zip *
 cd ..
 aws lambda update-function-code --function-name scanActivities --zip-file fileb://scanActivities.zip
 
+# get activities
+cd getActivities/
+zip -r ../getActivities.zip *
+cd ..
+aws lambda update-function-code --function-name getActivities --zip-file fileb://getActivities.zip
+
 echo "build complete"
