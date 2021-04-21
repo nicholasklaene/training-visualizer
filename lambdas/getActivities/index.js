d39d3731f00f3623b4f7e134e54f7228bce8935f
Nicholas Klaene <nicholasklaene@outlook.com>
2021-04-21 12:07:59 -0400

    finished getActivities lambda

--- lambdas/getActivities/index.js
+++ lambdas/getActivities/index.js
@@ -1,26 +1,46 @@
 const DynamoDB = require("aws-sdk/clients/dynamodb")
-const fetch = require("node-fetch")
 const dynamodb = new DynamoDB.DocumentClient({ region: "us-east-2" })
 
 exports.handler = async (event) => {
 
-    let date = (new Date()).getTime()
+    let page;
+
+    if(!event.queryStringParameters) {
+        console.log("in here, default!")
+        page = 1;
+    } else {
+        console.log("not the default!")
+        page = event.queryStringParameters.page ? event.queryStringParameters.page : 1
+    }
+
+    let startDate = new Date()
+    let endDate = new Date()
+    startDate = (startDate.setDate(startDate.getDate() - (7 * page))).valueOf() // 7 * page days ago
+    endDate = (endDate.setDate(endDate.getDate() - (7 * (page - 1)))).valueOf() // 7 * (page - 1) days ago
+
+    console.log("start time " + startDate + " end time: " + endDate)
 
     let params = {
         TableName: "training-visualizer",
-        Key: {
-            activity_id: activity.id
+        ProjectionExpression: "#date, activity_id, distance, elapsed_time, elevation_gain, moving_time, #name, polyline, start_latitude, start_longitude, #type",
+        FilterExpression: "#date between :start_time and :end_time",
+        ExpressionAttributeNames: { // date, name, type are reserved keywords
+            "#date": "date",
+            "#name": "name",
+            "#type": "type"
         },
-        AttributesToGet: [
-            'activity_id'
-        ]
+        ExpressionAttributeValues: {
+             ":start_time": startDate,
+             ":end_time": endDate 
+        }
     }
 
-    let result = await dynamodb.batchGet(params).promise()
+    let data = await dynamodb.scan(params).promise()
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(data.Items) // add json stringify
     };
+
     return response;
-};
+}
