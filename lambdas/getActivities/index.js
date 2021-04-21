e4ea52244a65d01d3eb386f77811979a1ff6af24
Nicholas Klaene <nicholasklaene@outlook.com>
2021-04-21 10:41:02 -0400

    changed date format for filtering purposes

--- lambdas/getActivities/index.js
+++ lambdas/getActivities/index.js
@@ -1,4 +1,22 @@
+const DynamoDB = require("aws-sdk/clients/dynamodb")
+const fetch = require("node-fetch")
+const dynamodb = new DynamoDB.DocumentClient({ region: "us-east-2" })
+
 exports.handler = async (event) => {
+
+    let date = (new Date()).getTime()
+
+    let params = {
+        TableName: "training-visualizer",
+        Key: {
+            activity_id: activity.id
+        },
+        AttributesToGet: [
+            'activity_id'
+        ]
+    }
+
+    let result = await dynamodb.batchGet(params).promise()
 
     const response = {
         statusCode: 200,
